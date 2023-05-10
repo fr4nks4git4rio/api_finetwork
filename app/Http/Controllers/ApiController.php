dc902c4e56aacd2fefeab304660cf1f8ff2f0128
Frank Garcia Albert <fr4nk.s4git4rio@gmail.com>
2023-05-10 07:47:52 -0500

    arreglos

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,13 +3,40 @@
 namespace App\Http\Controllers;
 
 use Carbon\Carbon;
+use GuzzleHttp\Client as HttpClient;
+use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class ApiController extends Controller
 {
+    private $host = 'https://wss.bpodigital.com/llama_finetwork.php?';
+    private $http;
+
+    /**
+     * DefaultController constructor.
+     * @param $http
+     */
+    public function __construct(HttpClient $http)
+    {
+        $this->http = $http;
+    }
     public function recibirInfoFacebook(Request $request)
     {
-        return response()->json($request->input());
+        $input = $request->input();
+        $url = $this->host;
+        Log::info("Data Recibida de Facebook");
+        Log::info($input);
+        $url .= 'phone='.$input['data']['phone'].'&name='.$input['data_exten']['name'].'&email='.$input['data_exten']['email'];
+
+        try{
+            $r = $this->http->get($url, ['auth' => ['artemisa_leads', 'Rki4yjLk^L%8']]);
+        }catch (GuzzleException $e){
+//      return $this->sendError($e->getMessage());
+            return json_encode(['error' => 'No se pudo comunicar con el servicio. Inténtelo mas tarde.']);
+        }
+
+        return json_encode(['success' => true, 'response' => 'ok']);
     }
 }
